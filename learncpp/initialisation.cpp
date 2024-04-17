f47127d8aa9cd77ff2a0d09ba5f1829d5de985b9
Beorlor <jbdurand2000@gmail.com>
2024-04-17 15:42:05 +0200

    double <<

--- learncpp/initialisation.cpp
+++ learncpp/initialisation.cpp
@@ -3,8 +3,8 @@
 int main()
 {
 	int test = 8;
-    std::cout << test;
+    std::cout << test << "\n";
 	int test2(9); //direct initialisation : hard to differentiate from function declaration like int x();
-    std::cout << test2;
+    std::cout << "test2: " << test2 << "\n";
     return 0;
 }
