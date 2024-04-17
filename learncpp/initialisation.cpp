1471eac1e6c2f8fee016141ad48f73dcef6d86c3
Beorlor <jbdurand2000@gmail.com>
2024-04-17 15:32:25 +0200

    list

--- learncpp/initialisation.cpp
+++ learncpp/initialisation.cpp
@@ -4,7 +4,7 @@ int main()
 {
 	int test = 8;
     std::cout << test;
-	int test2(9); //new way
+	int test2(9); //direct initialisation : hard to differentiate from function declaration like int x();
     std::cout << test2;
     return 0;
 }
