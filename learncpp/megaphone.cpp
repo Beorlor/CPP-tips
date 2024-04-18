c0d0319e9c4dedf76f69b93a2650c9b45c46e443
Beorlor <jbdurand2000@gmail.com>
2024-04-18 15:52:18 +0200

    string

--- learncpp/megaphone.cpp
+++ learncpp/megaphone.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 #include <cctype> // for toupper function
 
-int main(int argc, char* argv[]) {
-    if (argc > 1) {
-        for (int i = 1; i < argc; i++) {
-            for (int j = 0; argv[i][j]; j++) {
+int main(int argc, char* argv[])
+{
+    if (argc > 1)
+	{
+        for (int i = 1; i < argc; i++)
+		{
+            for (int j = 0; argv[i][j]; j++)
+			{
                 std::cout << char(toupper(argv[i][j]));
             }
         }
         std::cout << std::endl;
-    } else {
+    }
+	else
+	{
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
     }
     return 0;
