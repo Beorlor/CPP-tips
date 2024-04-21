1101a0582a8828dc41cba33eb4f9a7a2d646efaa
Beorlor <jbdurand2000@gmail.com>
2024-04-21 14:56:43 +0200

    megaphone

--- learncpp/megaphone.cpp
+++ learncpp/megaphone.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
-#include <cctype> // for toupper function
+#include <algorithm> // For std::transform
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
     if (argc > 1)
 	{
         for (int i = 1; i < argc; i++)
 		{
-            for (int j = 0; argv[i][j]; j++)
-			{
-                std::cout << char(toupper(argv[i][j]));
-            }
+            std::string str = argv[i];
+
+            std::transform(str.begin(), str.end(), str.begin(),
+                (int(*)(int))std::toupper);
+
+            std::cout << str;
         }
         std::cout << std::endl;
     }
